c796bc387def091d045214d642867510fe48c333
dwro0121 <dwro0121@gmail.com>
2021-01-02 21:12:16 +0900

    need decoder&header

--- model/centernet.py
+++ model/centernet.py
@@ -6,8 +6,22 @@ class Centernet(nn.Module):
     def __init__(self):
         super(Centernet, self).__init__()
         self.backbone = None
-        self.decoer = None
-        self.header = None
+        self.decoder = Centernet_decoder()
+        self.header = Centernet_header()
+
+    def forward(self, x):
+        return x
+
+class Centernet_decoder(nn.module):
+    def __init__(self):
+        super(Centernet_decoder, self).__init__()
+
+    def forward(self, x):
+        return x
+
+class Centernet_header(nn.module):
+    def __init__(self):
+        super(Centernet_decoder, self).__init__()
 
     def forward(self, x):
         return x
