eb35afdefb455b0cecd616ca35e6f06f3a31fd45
dwro0121 <dwro0121@gmail.com>
2021-01-24 13:47:00 +0900

    Final version most likely

--- utils/losses.py
+++ utils/losses.py
@@ -3,6 +3,7 @@ import torch.nn.functional as F
 
 
 def focal_loss(output, label, alpha=2, beta=4):
+    loss = .0
     output = output.permute(0, 2, 3, 1)
     pos_inds = label.eq(1).float()
     neg_inds = label.lt(1).float()
@@ -17,9 +18,9 @@ def focal_loss(output, label, alpha=2, beta=4):
     neg_loss = neg_loss.sum()
 
     if num_pos == 0:
-        loss = -neg_loss
+        loss += -neg_loss
     else:
-        loss = -(pos_loss + neg_loss) / num_pos
+        loss += -(pos_loss + neg_loss) / num_pos
     return loss
 
 
@@ -28,5 +29,5 @@ def l1_loss(output, label, mask):
     expand_mask = torch.unsqueeze(mask, -1).repeat(1, 1, 1, 2)
 
     loss = F.l1_loss(output * expand_mask, label * expand_mask, reduction='sum')
-    loss = loss / (mask.sum() + 1e-4)
+    loss = loss / (mask.sum() + 1e-12)
     return loss
