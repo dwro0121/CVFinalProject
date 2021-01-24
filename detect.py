75c1a89999ccc8f3a0e46fc5ff1386a378e37b50
dwro0121 <dwro0121@gmail.com>
2021-01-24 12:15:58 +0900

    Final Version most likely

--- detect.py
+++ detect.py
@@ -8,23 +8,23 @@ if __name__ == "__main__":
     input_shape = (416, 416, 3)
     test_annotation_path = 'data/test_annotation.txt'
     classes_path = 'data/class_name.txt'
-    img_path = 'data/example.jpg'
+    img_path = 'data/example/example1.jpg'
     class_names = get_classes(classes_path)
     print('class_names = {}'.format(class_names))
     num_classes = len(class_names)
 
     Cuda = True
+    backbone = 'resnet50'
 
-    # model_path = './logs/resnet18.pth'
-    model_path = './logs/resnet50.pth'
-    model = Centernet(num_classes, 'resnet50', pretrain=False)
+    model_path = './logs/'+backbone+'.pth'
+    model = Centernet(num_classes, backbone, pretrain=False)
     torch.load(model_path)
     model.load_state_dict(torch.load(model_path))
 
     if Cuda:
         model = model.cuda()
     img = Image.open(img_path)
-
+    model.eval()
     img = detect_img(model, img, class_names, Cuda)
     plt.imshow(img)
     plt.show()
